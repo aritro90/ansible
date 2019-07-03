7e6fd58c2226b52c4a1873f1ce6c094611e5dc62
Aritro <aritro.basu.engg@gmail.com>
2019-07-03 20:30:19 +0530

    added the input script

---
+++ CentosWithActiveDirectory/input.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+while [ "$choice" != "y" ]
+do
+  echo Enter IP address client machine
+  read -p 'IP Address: ' IP_ADD
+  echo Enter network prefix
+  read -p 'Prefix: ' IP_prefix
+  echo Enter gateway IP
+  read -p 'Gateway IP : ' IP_GW
+  echo Enter DNS IP
+  read -p 'DNS IP: ' IP_DNS
+  echo "Enter Domain( lowercase )"
+  read -p 'Domain: ' IP_DOM
+  echo "Enter Active Directory hostname (lowercase)"
+  read -p 'AD host: ' IP_HOST
+  echo Enter CLient hostname without appending the domain
+  read -p 'Client hostname: ' IP_CLIENT
+  echo Enter NTP Server IP
+  read -p 'NTP IP: ' IP_NTP
+  echo "=========================================="
+  echo "=========================================="
+  echo "=========================================="
+  echo CLient Machine IP : $IP_ADD
+  echo Network prefix : $IP_prefix
+  echo Gateway IP : $IP_gw
+  echo DNS IP : $IP_DNS
+  echo Domain : $IP_DOM
+  echo Active Directory hostname : $IP_HOST
+  echo CLient hostname : $IP_CLIENT
+  echo NTP Server IP: $IP_NTP
+  echo "Please confirm if the details are correct?(y/n)"
+  read -p '(y/n): ' choice
+  echo "=========================================="
+  echo "=========================================="
+  echo "=========================================="
+done
+#this will require minimum bash 4.x in the machine
+IP_DOM_UPPER=${IP_DOM^^}
+IP_HOST_UPPER=${IP_HOST^^}
+IP_HOST_DOM_UPPER="$IP_HOST_UPPER.$IP_DOM_UPPER"
+IP_CLIENT_FQDN="$IP_CLIENT.$IP_DOM"
